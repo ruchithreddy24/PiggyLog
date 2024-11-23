8ee253ab79d954c52b825c62c0f40aedd98d7917
ruchithdhoni <ruchithdhoni@gmail.com>
2024-11-23 10:21:25 +0530

    started the project piggyLog - test 2
    Test Commit

---
+++ piggyLog/src/main/java/com/piggylog/piggylog/PiggyLogApplication.java
@@ -0,0 +1,13 @@
+package com.piggylog.piggylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggyLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiggyLogApplication.class, args);
+	}
+
+}
